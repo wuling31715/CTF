3bda9b2b4c39f716227b52ca279d4429c90513b5
wuling31715 <wuling31715@gmail.com>
2020-03-22 16:24:03 +0800

    .

--- nmap/os.py
+++ nmap/os.py
@@ -1,18 +1,19 @@
 import nmap
 
-ip = '192.168.1.2'
+ip = '192.168.2.39'
 nm = nmap.PortScanner()
-nm.scan(ip, '-O')
-if 'osmatch' in nm[ip]:
-    for osmatch in nm[ip]['osmatch']:
-        print('----------------')
-        print('name: %s' % osmatch['name'])
-        print('accuracy: %s' % osmatch['accuracy'])
-        print('line: %s' % osmatch['line'])
-        if 'osclass' in osmatch:
-            for osclass in osmatch['osclass']:
-                print('type: %s' % osclass['type'])
-                print('vendor: %s' % osclass['vendor'])
-                print('osfamily: %s' % osclass['osfamily'])
-                print('osgen: %s' % osclass['osgen'])
-                print('accuracy: %s' % osclass['accuracy'])                
+print(nm.scan(ip, '-O'))
+
+# if 'osmatch' in nm[ip]:
+# for osmatch in nm[ip]['osmatch']:
+#     print('----------------')
+#     print('name: %s' % osmatch['name'])
+#     print('accuracy: %s' % osmatch['accuracy'])
+#     print('line: %s' % osmatch['line'])
+#     if 'osclass' in osmatch:
+#         for osclass in osmatch['osclass']:
+#             print('type: %s' % osclass['type'])
+#             print('vendor: %s' % osclass['vendor'])
+#             print('osfamily: %s' % osclass['osfamily'])
+#             print('osgen: %s' % osclass['osgen'])
+#             print('accuracy: %s' % osclass['accuracy'])                
