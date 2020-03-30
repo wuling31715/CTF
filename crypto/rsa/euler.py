a1b1b837b096aff60e223cc5d871fa70ef32e102
wuling31715 <wuling31715@gmail.com>
2020-03-31 03:12:01 +0800

    .

--- crypto/rsa/euler.py
+++
@@ -1,2 +0,0 @@
-def euler_function(p, q):
-    return (p - 1) * (q - 1)
--- crypto/rsa/gcd.py
+++
@@ -1,5 +0,0 @@
-def greatest_common_divisor(a, b):
-    if b == 0:
-        return a
-    else:
-        return greatest_common_divisor(b, a % b)
