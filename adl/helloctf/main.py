f024bc21cdc4e04f5c1ee9032a1533fb49e9d1dd
wuling31715 <wuling31715@gmail.com>
2020-04-07 04:43:26 +0800

    .

--- adl/helloctf/main.py
+++ adl/helloctf/main.py
@@ -2,8 +2,9 @@ from pwn import *
 import struct
 
 print(cyclic(100))
-ip = 'ctf.adl.tw'
-port = 11001
-r = remote(ip, port)
-r.interactive()
+print(cyclic_find(b'l', n=1))
+# ip = 'ctf.adl.tw'
+# port = 11001
+# r = remote(ip, port)
+# r.interactive()
 
