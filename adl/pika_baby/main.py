7c8e291fed00608218136dbf24484ae6ca514160
wuling31715 <wuling31715@gmail.com>
2020-06-01 16:33:58 +0800

    .

---
+++ adl/pika_baby/main.py
@@ -0,0 +1,19 @@
+# view-source:http://140.115.59.7:12002/login.php.back
+
+user = "pikachu"
+pass = "s214587387a"
+
+# <?php
+#     $flag = "adlctf{pika_pika_pikachu}";
+#     $user = $_GET['user'];
+#     $pass = $_GET['pass'];
+#     if (!isset($user) || !isset($pass)) {
+#         header('Location', "./secret.html");
+#     } else {
+#         if ($user == "pikachu" && md5($pass) == "0e481756596645574257920728035178") {
+#             $text = $flag;
+#         } else {
+#             $text = "PIKA PIKA.";
+#         }
+#     }
+# ?>
